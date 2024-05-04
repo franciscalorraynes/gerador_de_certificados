70f1e54385410045b590ecff0f945fd211b11ee0
franciscalorraynes <franciscalorrayne5@gmail.com>
2024-05-03 21:06:06 -0300

    gerador_de_certificado_simples creation

---
+++ Word/gerador_de_certificados_simples.py
@@ -0,0 +1,19 @@
+from docx import Document
+from docx.shared  import Pt  # para aumentar e diminuir o texto
+
+# abrindo o arquivo word
+file_word = Document(r"C:\Users\fllsa\Desktop\Curso de RPA\Word\Certificado1.docx")
+# tamanho
+style_file = file_word.styles["Normal"]
+
+# subistintuindo o "Nome" e subistituir pelo nome desejado
+for paragraph_file in file_word.paragraphs:
+    if "@nome" in paragraph_file.text:
+        paragraph_file.text = "Francisca Lorrayne"
+        source = style_file.font
+        source.name = "Calibri (Corpo)"
+        style_file.size = Pt(24)
+
+# Salva o certificado com o nome do aluno
+file_word.save("Francisca Lorrayne.docx")
+print("Arquivo gerado com sucesso!")
